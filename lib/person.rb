77af34b0d2d227b80854e5ab45a4fb95c64d56a0
Tyler Firth <tyfirth@gmail.com>
2020-06-11 02:32:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,9 +3,9 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize
-  
-end
+  def initialize
+    
+  end
   
   
   
